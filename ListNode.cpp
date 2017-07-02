bd7c0eab74323780817893201361240bedbc69e8
張君瑞 <zhangjunrui1996@Gmail.com>
2017-07-02 21:59:54 +0800

    链表中插入，删除节点
    【注】：1.其实creat链表的时候没有必要传入二级指针或一级指针的引用，直接return结点指针就行。
    2.插入删除的时候传入一级指针即可。

--- ListNode.cpp
+++ ListNode.cpp
@@ -8,10 +8,10 @@ int data;
 struct LNode *next;
 }LNode, *LinkedList;
 
-LinkedList creatlist_tail_insert()
+LinkedList creatlist_tail_insert(LinkedList &L)
 {
     int temp_receive;
-    LinkedList L;
+//    LinkedList L;
     L=(LinkedList)malloc(sizeof(LNode));
     L->next=NULL;
     LNode *s,*r;
@@ -63,16 +63,46 @@ int print_list(LinkedList &L)
     return 1;
 }
 
-//LinkedList insert(LinkedList &L,)
-//{
-//
-//}
+LinkedList insertlist(LinkedList L,int loc,int X)
+{
+    LNode *p=L;
+    //printf("%d\n",p->data);
+    for(int i=1;i<loc;i++)
+        p=p->next;
+    LNode *s;
+    s=(LNode *)malloc(sizeof(LNode));
+    s->data=X;
+    s->next=p->next;
+    p->next=s;
+    return L;
+}
+
+LinkedList deleteNode(LinkedList L,int loc)
+{
+    LNode *p=L;
+    LNode *temp;
+    for(int i=1;i<loc;i++)
+        p=p->next;
+    temp=p->next;
+    p->next=p->next->next;
+    free(temp);
+    return L;
+}
 
 int main()
 {
-    LinkedList lb=creatlist_tail_insert();;
+    int loc,X;
+    LinkedList lb=creatlist_tail_insert(lb);
 //    creatlist_tail_insert(la);
 
+    print_list(lb);
+    printf("请输入插入的位置和数值:");
+    scanf("%d%d",&loc,&X);
+    insertlist(lb,loc,X);
+    print_list(lb);
+    printf("请输入删除的位置:");
+    scanf("%d",&loc);
+    deleteNode(lb,loc);
     print_list(lb);
 
     return 0;
